9055a0f6eb85766acb4c845dec018c0bca633e11
H.M. Sarwer Alam <86118255+sarweralam867@users.noreply.github.com>
2025-06-26 00:32:40 +0600

    video 37 : Custom Widget build

--- my_first_project/lib/main.dart
+++ my_first_project/lib/main.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 83, 47, 146),
-                const Color.fromARGB(255, 132, 41, 148),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              "My First App",
-              style: TextStyle(color: Colors.white, fontSize: 35),
-            ),
-          ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(ctx) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 83, 47, 146),
+            const Color.fromARGB(255, 132, 41, 148),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
-    ),
-  );
+      child: Center(
+        child: Text(
+          "My First App",
+          style: TextStyle(color: Colors.white, fontSize: 35),
+        ),
+      ),
+    );
+  }
 }
