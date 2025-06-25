01d0eb1f9aa3d20f445734651338f3f54c82236b
H.M. Sarwer Alam <86118255+sarweralam867@users.noreply.github.com>
2025-06-26 00:11:57 +0600

    video 33

--- my_first_project/lib/main.dart
+++ my_first_project/lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 28, 157, 144),
-        body: Center(child: Text("My First App")),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 83, 47, 146),
+                const Color.fromARGB(255, 132, 41, 148),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(child: Text("My First App")),
+        ),
       ),
     ),
   );
