3beb0fc0991675c9bab17c68a3339ffbbdb77938
H.M. Sarwer Alam <86118255+sarweralam867@users.noreply.github.com>
2025-06-27 11:40:52 +0600

    Empty main

--- my_first_project/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:my_first_project/gradient_container.dart';
-
-void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
-}
