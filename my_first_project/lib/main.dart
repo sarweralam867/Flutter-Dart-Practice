6a9751481e3e7f37a3bf2de344b91c6b370c6a2a
H.M. Sarwer Alam <86118255+sarweralam867@users.noreply.github.com>
2025-06-27 11:42:54 +0600

    till video 39

---
+++ my_first_project/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_project/gradient_container.dart';
+
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
