71e5978794b41dab3f29f55ef8057c62860647b0
H.M. Sarwer Alam <86118255+sarweralam867@users.noreply.github.com>
2025-06-29 22:37:19 +0600

    Reuseable widget classes

--- my_first_project/lib/gradient_container2.dart
+++ my_first_project/lib/gradient_container2.dart
@@ -4,11 +4,12 @@ import 'package:my_first_project/style_test.dart';
 final startAlignment = Alignment.topLeft;
 const endAlignmnet = Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer2 extends StatelessWidget {
   // Constructor (Optional)
-  const GradientContainer({super.key, required this.colors});
+  const GradientContainer2(this.color1, this.color2, {super.key});
 
-  final List<Color> colors;
+  final Color color1;
+  final Color color2;
 
   @override
   @override
@@ -16,7 +17,7 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: colors,
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignmnet,
         ),
--- my_first_project/lib/main.dart
+++ my_first_project/lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_project/gradient_container.dart';
+import 'package:my_first_project/gradient_container2.dart';
+// import 'package:my_first_project/gradient_container.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer2(
+          const Color.fromARGB(255, 29, 8, 188),
+          const Color.fromARGB(255, 89, 55, 126),
+        ),
+      ),
+    ),
+  );
 }
